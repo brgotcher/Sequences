7066a1ac1a478f5a43cca722eb8fea2df8898fa7
brgotcher <brgotcher@gmail.com>
2020-09-12 06:01:12 -0500

    add splitting_things.py

---
+++ splitting_things.py
@@ -0,0 +1,30 @@
+panagram = """The quick brown 
+fox jumps\tover 
+the lazy dog"""
+
+words = panagram.split()
+print(words)
+
+numbers = "9,223,372,036,854,775,807"
+print(numbers.split(","))
+
+
+generated_list = ['9', ' ',
+                  '2', '2', '3', ' ',
+                  '3', '7', '2', ' ',
+                  '0', '3', '6', ' ',
+                  '8', '5', '4', ' ',
+                  '7', '7', '5', ' ',
+                  '8', '0', '7']
+values = "".join(generated_list)
+print(values)
+
+values_list = values.split()
+print(values_list)
+num = ",".join(values_list)
+print(num)
+
+int_list = []
+for n in values_list:
+    int_list.append(int(n))
+print(int_list)
